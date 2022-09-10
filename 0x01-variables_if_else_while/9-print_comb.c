4bc153ef691e10bd45b6ae723559b8875d2f50c7
Anne-Kabutu <annekabutu@gmail.com>
2022-09-10 04:48:18 -0700

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 		putchar((a % 10) + '0');
-		if (a == 9)
-		putchar(',');
-		putchar(' ');
+		if (a == 9)	
+	putchar(',');
+	putchar(' ');
 	putchar('\n');
 	return (0);
 }
