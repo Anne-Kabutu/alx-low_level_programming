f741ba9ea52df2d00a1b3ade8eaa2bc1df9411f4
Anne-Kabutu <annekabutu@gmail.com>
2022-09-09 22:35:14 -0700

    prints single digits of base 10

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,10 @@ int main(void)
 {
 	char a;
 
-	for (a = 'a'; a <= 'z', a++)
-		if (a == 'q'; a == 'e')
-			continue
-				putchar(a);
-	putchar('\n')
+	for (a = 'a'; a <= 'z'; a++)
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	putchar('\n');
 		return (0);
 }
 
