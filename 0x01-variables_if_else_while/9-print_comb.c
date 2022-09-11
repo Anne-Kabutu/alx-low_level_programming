9f01eb0b4c311b06d6ea3c82b32d899dd796edb4
Anne-Kabutu <annekabutu@gmail.com>
2022-09-10 21:18:13 -0700

    Combination of 3 numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,8 @@ int main(void)
 	int a;
 
 	for (a = 0; a <= 9; a++)
-		putchar((a % 10) + '0');
-		if (a == 9)	
+	putchar((a % 10) + '0');
+	if (a == 9)	
 	putchar(',');
 	putchar(' ');
 	putchar('\n');
