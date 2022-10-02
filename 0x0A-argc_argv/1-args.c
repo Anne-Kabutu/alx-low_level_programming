3f0b19d465843f25f2ce8a8d6a002d289f984b06
Anne-Kabutu <annekabutu@gmail.com>
2022-10-02 09:47:47 -0700

    mandatory tasks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#Include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: argument count
+ * @argv: argument vector of pointers to string
+ *
+ * Return: always 0
+ */
+int main(int argc, char* argv [] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
