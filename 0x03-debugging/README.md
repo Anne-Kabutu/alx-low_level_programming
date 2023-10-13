b3eca13cc8c178bb4b941239922bbaeedbb267fa
Anne-Kabutu <annekabutu@gmail.com>
2023-10-13 15:20:35 +0300

    positive or negative test file

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+learning how to manually debug
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+/* checks whether a num is positive or negative*/
+
+#endif /* MAIN_H */
