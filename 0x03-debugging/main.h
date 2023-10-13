a0b03607e6aeea00073e9f2e39925b0dd187ff2f
Anne-Kabutu <annekabutu@gmail.com>
2023-10-13 16:15:38 +0300

    largest of three nums .h prototype

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,7 @@
 void positive_or_negative(int i);
 /* checks if an int is positive or negative*/
 
+int largest_number(int a, int b, int c);
+/* returns the largest num of three nums*/
+
 #endif /* MAIN_H */
