ade83df2b0a042c1ff150ac48326f5fb64c98c28
Anne-Kabutu <annekabutu@gmail.com>
2023-10-17 21:06:54 +0300

    corrections

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,26 +11,17 @@ void print_rev(char *s)
 {
 	int index = 0;
 
-	if (*(s + index) == '\0')
+	while (*(s + index) != '\0')
 	{
-		_putchar('\0');
+		index++;
 	}
 
-	else
+	index--;
+
+	while (!(index < 0))
 	{
-
-		while (*(s + index) != '\0')
-		{
-			index++;
-		}
-
+		_putchar(*(s + index));
 		index--;
-
-		while (!(index < 0))
-		{
-			_putchar(*(s + index));
-			index--;
-		}
 	}
 
 	_putchar('\n');
