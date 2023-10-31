c7cc912b399cd83b8a1abffa42b67cffc7d3952e
Anne-Kabutu <annekabutu@gmail.com>
2023-10-31 10:41:11 +0300

    checking return of malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,10 +19,17 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(sizeof(char) * size);
 
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
 
 	return (arr);
+	free(arr);
 }
