5fb76392c1af5a20b97d48d8be66bc252d224d23
Anne-Kabutu <annekabutu@gmail.com>
2023-11-03 17:53:35 +0300

    correcting indexing for null char in newstr

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,10 +6,8 @@
  * @s1: first string
  * @s2: second string
  * @n: size of string two to copy
- *
  * Return: NULL for failure, or ponter to new mem
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, index, size, cnt;
@@ -18,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 ==NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
@@ -27,38 +25,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (index = 0; s2[index] != '\0'; index++)
 		;
 
-	if (n < index)
-	{
+	if (index > n)
 		size = i + n;
-	}
 
-	if (n >= index)
-	{
+	else
 		size = index + i;
-	}
 
 	newstr = malloc(sizeof(char) * (size + 1));
 
 	if (newstr == NULL)
-	{
-		free(newstr);
-		return(NULL);
-	}
+		return (NULL);
 
 	for (cnt = 0; cnt < i; cnt++)
-	{
 		newstr[cnt] = s1[cnt];
-	}
 
 	i = 0;
-
 	for (; cnt < size; cnt++)
 	{
 		newstr[cnt] = s2[i];
 		i++;
 	}
 
-	newstr[i] = '\0';
+	newstr[cnt] = '\0';
 
 	return (newstr);
+	free(newstr);
 }
