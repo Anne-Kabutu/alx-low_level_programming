cb2d4efcc1d185a6cfe9a31254f147a7a190f2fa
Anne-Kabutu <annekabutu@gmail.com>
2023-11-07 22:52:49 +0300

    creating structs, ponters to structs, allocating dynamic mem for structs and freeing, and using typedef for user defined types

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * new_dog - creates a new dog struct type
@@ -12,20 +12,40 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *newname, *newowner;
-	dog_t newdog, *dogptr;
+	dog_t *dogptr;
+	int i, index, count;
 
-	newname = name;
-	newowner = owner;
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (index = 0; owner[index] != '\0'; index++)
+		;
 
-	newdog.name = newname;
-	newdog.owner = newowner;
-	newdog.age = age;
+	dogptr = malloc(sizeof(dogptr));
 
-	dogptr = &newdog;
-
-	if (dogptr == NULL)
+	if (dogptr == NULL || !(name) || !(owner))
+	{
+		free(dogptr);
 		return (NULL);
+	}
+
+	dogptr->name = malloc(sizeof(char) * (i + 1));
+	dogptr->owner  = malloc(sizeof(char) * (index + 1));
+
+	if (!(dogptr->name) || !(dogptr->owner))
+	{
+		free(dogptr->owner);
+		free(dogptr->name);
+		free(dogptr);
+		return (NULL);
+	}
+	for (count = 0; count < i; count++)
+		dogptr->name[count] = name[count];
+	dogptr->name[count] = '\0';
+
+	for (count = 0; count < index; count++)
+		dogptr->owner[count] = owner[count];
+	dogptr->owner[count] = '\0';
+	dogptr->age = age;
 
 	return (dogptr);
 }
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,38 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - contains dog data
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: demographic dog data
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+/* intializes struct dog*/
+
+void print_dog(struct dog *d);
+/*  prints a struct dog*/
+
+dog_t *new_dog(char *name, float age, char *owner);
+/* creates a new dog*/
+
+void free_dog(dog_t *d);
+/* frees dogs*/
+
+#endif /* _DOG_H_*/
